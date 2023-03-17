803e60a4e9846a88de65d2fcbda8b6f724a4ff7c
Gaurav <gaurav.inamdar@hotmail.com>
2023-03-17 12:18:11 +0530

    added webdriver_keywords.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,23 +1,29 @@
 from selenium.webdriver.common.by import By
 
+from base.webdriver_keywords import WebDriverKeywords
 
-class LoginPage:
+
+class LoginPage(WebDriverKeywords):
 
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
+        # self.driver = driver
 
     def enter_username(self, username):
-        self.driver.find_element(By.NAME, "username").send_keys(username)
+        # self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.type_by_locator((By.NAME, "username"),username)
 
     def enter_password(self, password):
-        self.driver.find_element(By.NAME, "password").send_keys(password)
+      #  self.driver.find_element(By.NAME, "password").send_keys(password)
+        self.type_by_locator((By.NAME, "password"),password)
 
     def click_on_login(self):
-        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        # self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        self.type_by_locator((By.XPATH, "//button[@type='submit']"))
 
     @property
     def invalid_login_error(self):
-        return self.driver.find_element(By.XPATH,"//p[normalize-space() = 'Invalid credentials']").text
+        return self.driver.find_element(By.XPATH, "//p[normalize-space() = 'Invalid credentials']").text
 
     @property
     def get_username_placeholder(self):
